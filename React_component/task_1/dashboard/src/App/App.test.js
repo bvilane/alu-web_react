bf169e4dd5881810fa7bf501756476fd974edbc3
bvilane <b.vilane@alustudent.com>
2025-05-24 18:50:42 +0200

    Trying to fix error

--- React_component/task_1/dashboard/src/App/App.test.js
+++ React_component/task_1/dashboard/src/App/App.test.js
@@ -12,70 +12,60 @@ import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
 
 describe('<App />', () => {
-    it('renders an <App /> component', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper).toHaveLength(1);
-    });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
 
-    it('renders <Notifications />', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Notifications)).toHaveLength(1);
-    });
+  it('renders an <App /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toHaveLength(1);
+  });
 
-    it('renders <Header />', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Header)).toHaveLength(1);
-    });
+  it('renders <Notifications />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Notifications)).toHaveLength(1);
+  });
 
-    it('renders <Login /> when isLoggedIn is false', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Login)).toHaveLength(1);
-    });
+  it('renders <Header />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Header)).toHaveLength(1);
+  });
 
-    it('does not render <CourseList /> when isLoggedIn is false', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(CourseList)).toHaveLength(0);
-    });
+  it('renders <Login /> when isLoggedIn is false', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Login)).toHaveLength(1);
+  });
 
-    it('renders <Footer />', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find(Footer)).toHaveLength(1);
-    });
+  it('does not render <CourseList /> when isLoggedIn is false', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+  });
 
-    it('does not render <Login /> when isLoggedIn is true', () => {
-        const wrapper = shallow(<App isLoggedIn={true} />);
-        expect(wrapper.find(Login)).toHaveLength(0);
-    });
+  it('renders <Footer />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(Footer)).toHaveLength(1);
+  });
 
-    it('renders <CourseList /> when isLoggedIn is true', () => {
-        const wrapper = shallow(<App isLoggedIn={true} />);
-        expect(wrapper.find(CourseList)).toHaveLength(1);
-    });
+  it('does not render <Login /> when isLoggedIn is true', () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.find(Login)).toHaveLength(0);
+  });
 
-    it('verifies that the user can log out using ctrl + h', () => {
-        const events = {};
-        window.addEventListener = jest.fn().mockImplementation((e, cb) => {
-            events[e] = cb;
-        });
+  it('renders <CourseList /> when isLoggedIn is true', () => {
+    const wrapper = shallow(<App isLoggedIn={true} />);
+    expect(wrapper.find(CourseList)).toHaveLength(1);
+  });
 
-        const props = {
-            isLoggedIn: true,
-            logOut: jest.fn(),
-        };
+  it('verifies that the user can log out using ctrl + h', () => {
+    const logOutMock = jest.fn();
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation(() => {});
 
-        window.alert = jest.fn();
+    const wrapper = mount(<App isLoggedIn={true} logOut={logOutMock} />);
 
-        // Use `mount` instead of `shallow` for lifecycle methods
-        const wrapper = mount(<App {...props} />);
+    const event = new KeyboardEvent('keydown', { key: 'h', ctrlKey: true });
+    window.dispatchEvent(event);
 
-        // Simulate the keydown event
-        events.keydown({ ctrlKey: true, key: 'h' });
-
-        // Assertions
-        expect(window.alert).toHaveBeenCalledWith("Logging you out");
-        expect(props.logOut).toHaveBeenCalled();
-
-        // Restore mocks
-        window.alert.mockRestore();
-    });
+    expect(alertMock).toHaveBeenCalledWith('Logging you out');
+    expect(logOutMock).toHaveBeenCalled();
+  });
 });
